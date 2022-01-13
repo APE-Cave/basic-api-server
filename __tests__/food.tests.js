f287dc8369eaf0f46fca5a05e6ab219181fc8fc8
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-01-12 23:04:10 -0800

    Adds foodSchema

--- __tests__/food.tests.js
+++ __tests__/food.tests.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const {app} = require('./lib/server.js');
+const {app} = require('../lib/server.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
 describe('Testing the food router', () => {
-  it('Should read from food data', () => {
-    const response = request.get('/food');
+
+  it('Should read from food data', async () => {
+  
+    const response = await request.get('/food');
     expect(response.status).toEqual(200);
     expect(response.body.count).toBeDefined;
     expect(response.body.results).toBeDefined;
-  })
+  });
 });
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const { start, app } = require('./lib/server.js');
+
+const PORT = process.env.PORT || 3000;
+
+start(PORT);

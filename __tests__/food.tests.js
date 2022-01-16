1dc3f121a77023ee5cd7513ee9f9e5522041da8c
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-01-16 15:41:32 -0800

    Completes and passes CREATE and READ food tests

--- __tests__/food.tests.js
+++ __tests__/food.tests.js
@@ -3,14 +3,33 @@
 const {app} = require('../lib/server.js');
 const supertest = require('supertest');
 const request = supertest(app);
+const { db } = require('../lib/models');
+
+beforeAll(async () => {
+  // await db.drop();  
+  await db.sync();
+});
+afterAll(async () => {
+  await db.drop();
+});
 
 describe('Testing the food router', () => {
 
-  it('Should read from food data', async () => {
+  it('Should create new food data', async () => {
+    const testObj = {'dishName': 'wings', 'quantity': '24'};
+    // const testObj = {'dishName': 'salad', 'quantity': '3'};
+    // const testObj = {'dishName': 'soup', 'quantity': '5'};
+    const response = await request.post('/food').send(testObj);
+    expect(response.status).toEqual(200);
+    console.log(response.body);
+    expect(response.body.quantity).toEqual(testObj.quantity);
+    expect(response.body.dishName).toEqual(testObj.dishName);
+  });
   
+  it('Should read from food data', async () => {
     const response = await request.get('/food');
     expect(response.status).toEqual(200);
-    expect(response.body.count).toBeDefined;
+    expect(response.body.count).toEqual(1);
     expect(response.body.results).toBeDefined;
   });
 });

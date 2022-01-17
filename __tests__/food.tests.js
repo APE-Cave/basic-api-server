48c53583ccaedfdb782c1eb8c877877b9a9b7f11
Andrew Enyeart <reallyhardtoforget@gmail.com>
2022-01-16 18:36:08 -0800

    About to refactor food.js and tests to omit count

--- __tests__/food.tests.js
+++ __tests__/food.tests.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const {app} = require('../lib/server.js');
+const { app } = require('../lib/server.js');
 const supertest = require('supertest');
 const request = supertest(app);
 const { db } = require('../lib/models');
 
 beforeAll(async () => {
-  // await db.drop();  
+  await db.drop();  
   await db.sync();
 });
 afterAll(async () => {
@@ -15,21 +15,35 @@ afterAll(async () => {
 
 describe('Testing the food router', () => {
 
-  it('Should create new food data', async () => {
-    const testObj = {'dishName': 'wings', 'quantity': '24'};
-    // const testObj = {'dishName': 'salad', 'quantity': '3'};
+  it('Should create new food data', () => {
+    // const testObj = {'dishName': 'wings', 'quantity': '24'};
     // const testObj = {'dishName': 'soup', 'quantity': '5'};
-    const response = await request.post('/food').send(testObj);
-    expect(response.status).toEqual(200);
-    console.log(response.body);
-    expect(response.body.quantity).toEqual(testObj.quantity);
-    expect(response.body.dishName).toEqual(testObj.dishName);
+    const testArray = [
+      { 'dishName': 'wings', 'quantity': '24' },
+      { 'dishName': 'soup', 'quantity': '5' },
+      { 'dishName': 'salad', 'quantity': '3' },
+    ];
+    testArray.forEach(async (testObj) => {
+      const response = await request.post('/food').send(testObj);
+      console.log(response.body);
+      expect(response.status).toEqual(200);
+      expect(response.body.quantity).toEqual(testObj.quantity);
+      expect(response.body.dishName).toEqual(testObj.dishName);
+    });
   });
-  
-  it('Should read from food data', async () => {
+
+  it('Should read all from food data', async () => {
     const response = await request.get('/food');
     expect(response.status).toEqual(200);
-    expect(response.body.count).toEqual(1);
+    expect(response.body.count).toEqual(3);
     expect(response.body.results).toBeDefined;
+  });
+  
+  it('Should read one from food data by id', async () => {
+    const response = await request.get('/food/1');
+    expect(response.status).toEqual(200);
+    expect(response.body.count).toEqual(1);
+    expect(response.body.results.id).toEqual(1);
+
   });
 });
